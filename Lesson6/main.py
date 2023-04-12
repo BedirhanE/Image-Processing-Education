dc843e89f91139baf29235943f19a3ce861e7c20
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-04-12 03:56:21 +0300

    Kameradan görüntü okuma işlemi

--- Lesson6/main.py
+++ Lesson6/main.py
@@ -1,1 +1,15 @@
 
+import cv2
+
+kamera = cv2.VideoCapture(0)
+
+
+#while döngüsü içerisinde  read fonksiyonunu kullanarak kameradaki  görüntüyü okuma işlemi yapıyoruz
+while (True):
+    ret, videoGoruntu = kamera.read()
+    cv2.imshow("Bilgisayar Kamerasi", videoGoruntu)
+    if cv2.waitKey(50) & 0xFF == ord('x'):
+        break
+
+kamera.release()
+cv2.destroyAllWindows()
