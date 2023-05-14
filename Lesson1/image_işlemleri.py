b1137057b85fb3ac7e2fce792221712cb5f18463
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-05-14 23:58:44 +0300

    Image Processing

--- Lesson1/image_işlemleri.py
+++ Lesson1/image_işlemleri.py
@@ -1,9 +1,9 @@
 #kullanıcağım OpenCV kütüphanesini import etttim.
 import cv2
 
-image = cv2.imread("doga.jpg") #resmi dosyadan orjinal hali ile okuma işlemi
+image = cv2.imread("doga.jpg") #resmi dosyadan orjinal hali ile okuma işlemi.
 cv2.imshow("doga",image)  #resmi ekranda gösterme işlemi
-#cv2.imwrite("doga.png",resim) #png uzantılı resim oluşturma işlemi
+#cv2.imwrite("doga.png",resim) #png uzantılı resim oluşturma işlemi.
 
 #resimdeki RGB özelliklerini incelemek için
 #resmimde  herhangi bir pikselin renk değerini görüntülemek için
@@ -17,6 +17,6 @@ genislik = image.shape[1]
 print('Resim Yüksekliği (px)      :', yukseklik)
 print('Resim Genişliği  (px)     :', genislik)
 
-cv2.waitKey(0) # resmimi pencerede sabit olarak kalması  ve herhangi bir tuşabasınca kaybolması işlemini gerçekleştirdik
+cv2.waitKey(0) # resmimi pencerede sabit olarak kalması  ve herhangi bir tuşabasınca kaybolması işlemini gerçekleştirdik.
 cv2.destroyAllWindows()
 #resim = cv2.imread("everest.jpg",0) #resme sıfır parametresini göndererek resmi gri tonda filtrelemiş oluyorum.
