1128d714dc32259f32745f7d86fc57934ec4921a
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-12-25 16:40:02 +0300

    Update main.py

--- Lesson6/main.py
+++ Lesson6/main.py
@@ -1,5 +1,5 @@
 
-import cv2
+"""import cv2
 
 kamera = cv2.VideoCapture(0)
 
@@ -13,4 +13,14 @@ while (True):
 
 cv2.VideoCapture(0).release()#kamerayı serbest bırakıyoruz
 kamera.release()#kamerayı serbest bırakma işlemi.
-cv2.destroyAllWindows()#tüm pencereleri kapatma işlemi.
+cv2.destroyAllWindows()#tüm pencereleri kapatma işlemi."""
+
+import tensorflow as tf
+# Import TensorFlow Datasets
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# Helper libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
